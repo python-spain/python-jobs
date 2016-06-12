02d3ebd7cdda537dd644716899cd06a6453fe098
Fede Mon <gnu.fede@gmail.com>
2016-06-12 21:01:58 +0200

    EMAIL config

--- python_jobs/settings.py
+++ python_jobs/settings.py
@@ -205,7 +205,9 @@ class Prod(Base):
     }
 
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = 'mail.google.com'
+    EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = 'jobspythonmadrid@gmail.com'
     EMAIL_HOST_PASSWORD = values.Value('password')
     EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_USE_SSL = True
