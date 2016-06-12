40c419718f1cf570c67cd1ea3ada8fbbc0796d90
Fede Mon <gnu.fede@gmail.com>
2016-06-12 21:40:28 +0200

    SSL and TLS are mutually exclusive

--- python_jobs/settings.py
+++ python_jobs/settings.py
@@ -202,7 +202,6 @@ class Prod(Base):
     EMAIL_HOST_PASSWORD = values.Value('password')
     EMAIL_PORT = 587
     EMAIL_USE_TLS = True
-    EMAIL_USE_SSL = True
 
     RQ_QUEUES = {
         'default': {
